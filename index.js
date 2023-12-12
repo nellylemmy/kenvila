119dd8b4a962d5d99490b4bee6ca627ad8fd72d6
nelly <nelsonlemmy61@gmail.com>
2023-12-13 00:53:09 +0300

    important updates

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const server = http.createServer(app);
 const io = socketIO(server);
 const session = require('express-session');
 const rateLimit = require('express-rate-limit');
+const bcrypt = require('bcryptjs');
 
 require('dotenv').config();
 
@@ -24,211 +25,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const rateLimiterMessage = `<style>
 section{
-    max-width:30rem;
-    margin:5rem auto;
+max-width:30rem;
+margin:5rem auto;
 }
 .card {
-    display: flex;
-    flex-direction: column;
-    border: 1px red solid;
+display: flex;
+flex-direction: column;
+border: 1px red solid;
 }
 .header {
-    height: 30%;
-    background: red;
-    color: white;
-    text-align: center;
-    font-size: 1.3rem;
-    font-weight: 600;
+height: 30%;
+background: red;
+color: white;
+text-align: center;
+font-size: 1.3rem;
+font-weight: 600;
 }
 .container {
-    padding: 2px 16px;
+padding: 2px 16px;
 }
 </style>
 
 <section>
 <div class="card">
 <div class="header">
-    <p>Too Many Request! Please Wait for 15 minutes</p>
+<p>Too Many Request! Please Wait for 15 minutes</p>
 </div>
-    <div class="container">
-    <p>We apologize for the inconvenience, but it seems that there has been unusual activity detected from your device. To ensure the security of your account, we have temporarily restricted access for the next 15 minutes. This precautionary measure helps us protect your account from potential unauthorized access.
-    </p>
+<div class="container">
+<p>We apologize for the inconvenience, but it seems that there has been unusual activity detected from your device. To ensure the security of your account, we have temporarily restricted access for the next 15 minutes. This precautionary measure helps us protect your account from potential unauthorized access.
+</p>
 
-    <p>
-    If you have forgotten your account information or need assistance, please don't hesitate to contact our support team. We'll be happy to assist you in recovering your account.
-    </p>
-    
-    <p>Thank you for your understanding and cooperation in maintaining the security of your account.</p>
-</div>
-</div>
-</section>`;
+<p>
+If you have forgotten your account information or need assistance, please don't hesitate to contact our support team. We'll be happy to assist you in recovering your account.
+</p>
 
-const noAccountWithThatPhoneNumberMessage = `<style>
-section{
-    max-width:30rem;
-    margin:5rem auto;
-}
-.card {
-    display: flex;
-    flex-direction: column;
-    border: 1px red solid;
-}
-.header {
-    height: 30%;
-    background: red;
-    color: white;
-    text-align: center;
-    font-size: 1.3rem;
-    font-weight: 600;
-}
-.container {
-    padding: 2px 16px;
-}
-</style>
-
-<section>
-<div class="card">
-<div class="header">
-    <p>Phone Number Not Registered!</p>
-</div>
-    <div class="container">
-    <p>Hello there, The phone number provided is not in our system. Please SignUp or LogIn with another phone number.
-    </p>
-
-    <p>
-    If you have forgotten your account information or need assistance, please contact our support team. We'll be happy to assist you in recovering your account.
-    </p>
-    
-    <p><a href="/login.html">Login</a></p>
-    <p><a href="/signup.html">Signup</a></p>
+<p>Thank you for your understanding and cooperation in maintaining the security of your account.</p>
 </div>
 </div>
 </section>`;
-
-
-app.use((req, res, next) => {
-  res.status(404).send(`<style>body {
-      background-color: #2F3242;
-    }
-    svg {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      margin-top: -250px;
-      margin-left: -400px;
-    }
-    .message-box {
-      height: 200px;
-      width: 380px;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      margin-top: -100px;
-      margin-left: 50px;
-      color: #FFF;
-      font-family: Roboto;
-      font-weight: 300;
-    }
-    .message-box h1 {
-      font-size: 60px;
-      line-height: 46px;
-      margin-bottom: 40px;
-    }
-    .buttons-con .action-link-wrap {
-      margin-top: 40px;
-    }
-    .buttons-con .action-link-wrap a {
-      background: #68c950;
-      padding: 8px 25px;
-      border-radius: 4px;
-      color: #FFF;
-      font-weight: bold;
-      font-size: 14px;
-      transition: all 0.3s linear;
-      cursor: pointer;
-      text-decoration: none;
-      margin-right: 10px
-    }
-    .buttons-con .action-link-wrap a:hover {
-      background: #5A5C6C;
-      color: #fff;
-    }
-    
-    #Polygon-1 , #Polygon-2 , #Polygon-3 , #Polygon-4 , #Polygon-4, #Polygon-5 {
-      animation: float 1s infinite ease-in-out alternate;
-    }
-    #Polygon-2 {
-      animation-delay: .2s; 
-    }
-    #Polygon-3 {
-      animation-delay: .4s; 
-    }
-    #Polygon-4 {
-      animation-delay: .6s; 
-    }
-    #Polygon-5 {
-      animation-delay: .8s; 
-    }
-    
-    @keyframes float {
-        100% {
-        transform: translateY(20px);
-      }
-    }
-    @media (max-width: 450px) {
-      svg {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-top: -250px;
-        margin-left: -190px;
-      }
-      .message-box {
-        top: 50%;
-        left: 50%;
-        margin-top: -100px;
-        margin-left: -190px;
-        text-align: center;
-      }
-    }</style><svg width="380px" height="500px" viewBox="0 0 837 1045" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
-  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
-      <path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Polygon-1" stroke="#007FB2" stroke-width="6" sketch:type="MSShapeGroup"></path>
-      <path d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z" id="Polygon-2" stroke="#EF4A5B" stroke-width="6" sketch:type="MSShapeGroup"></path>
-      <path d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z" id="Polygon-3" stroke="#795D9C" stroke-width="6" sketch:type="MSShapeGroup"></path>
-      <path d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z" id="Polygon-4" stroke="#F2773F" stroke-width="6" sketch:type="MSShapeGroup"></path>
-      <path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Polygon-5" stroke="#36B455" stroke-width="6" sketch:type="MSShapeGroup"></path>
-  </g>
-</svg>
-<div class="message-box">
-<h1>404</h1>
-<p>Page not found</p>
-<div class="buttons-con">
-  <div class="action-link-wrap">
-    <a onclick="history.back(-1)" class="link-button link-back-button">Go Back</a>
-  </div>
-</div>
-</div>`)
-})
-
-
-let rateLimitMessage = {
-  windowMs: 3 * 60 * 1000, // 6 minutes
-  max: 100, // Limit each IP to 20 requests per `window` (here, per 6 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: ({ success: false, message: rateLimiterMessage })
-};
-// Error 429 (Too Many Requests) We detected that you sent wrong account information and we intentionally decide to freeze your system for a short period. please try again later. 
-const userLoginAttemptLimiter = rateLimit(rateLimitMessage);
-
-
-
-// // Middleware for session handling
-// app.use(session({
-//   secret: 'your-secret-key',
-//   resave: false,
-//   saveUninitialized: true,
-// }));
 
 app.use(session({
   name: 'session',
@@ -241,24 +75,40 @@ app.use(session({
     // sameSite: 'strict' // Enforce strict same-site policy for the session cookie
   }
 }));
+let rateLimitMessage = {
+  windowMs: 3 * 60 * 1000, // 6 minutes
+  max: 100, // Limit each IP to 20 requests per `window` (here, per 6 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: ({ success: false, message: rateLimiterMessage })
+};
 
 // // Rate limiting middleware
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 2, // limit each IP to 50 requests per windowMs
-//   message: 'Too many requests from this IP, please try again after some time.',
-// });
-// app.use('/login', limiter);
+const userLoginAttemptLimiter = rateLimit(rateLimitMessage);
 app.use('/login', (userLoginAttemptLimiter))
 
-// Set up a simple HTML form for login
-app.get('/worker/login', (req, res) => {
-  // Check if the worker is already logged in
-  if (req.session.worker) {
-    return res.redirect('/worker/dashboard');
+// generate argent number
+let generateRandomNumbers = function (amount, limit) {
+  var result = [],
+    memo = {};
+
+  while (result.length < amount) {
+    var num = Math.floor((Math.random() * limit) + 1);
+    if (!memo[num]) { memo[num] = num; result.push(num); };
   }
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
+  return result;
+}
+
+
+
+// Set up a simple HTML form for login
+// app.get('/worker/login', (req, res) => {
+//   // Check if the worker is already logged in
+//   if (req.session.worker) {
+//     return res.redirect('/worker/dashboard');
+//   }
+//   res.sendFile(path.join(__dirname, 'public', 'login.html'));
+// });
 
 // Set up a simple HTML form for signup
 app.get('/worker/signup', (req, res) => {
@@ -279,7 +129,7 @@ app.get('/', (req, res) => {
     // return res.json({ success: true, message: 'redirecting worker to main dashboard...' });
     return res.redirect('/worker/dashboard');
     // return res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
-  }else{
+  } else {
     // return res.json({ success: false, message: 'Worker Must Log in first...' });
     return res.redirect('/worker');
     // return res.sendFile(path.join(__dirname, 'public', 'home.html'));
@@ -299,47 +149,56 @@ app.get('/worker/dashboard', (req, res) => {
 
 // Handle POST requests for signup
 app.post('/signup', async (req, res, next) => {
-  const { workerFirstName, workerLastName, workerMobile } = req.body;
-    
-try {
-        
-const [row] = await dbConnection.execute(
-  "SELECT * FROM `workers` WHERE `worker_mobile_number`=?",
-  [workerMobile]
-  );
-            
-if (row.length >= 1) {
-  console.log('This Phone Number has already been used.');
-  return res.json({ success: false, message: 'This Phone Number has already been used.' });
-}
-        
-// const [rowEmail] = await dbConnection.execute(
-//   "SELECT * FROM `users` WHERE `email`=?",
-//   [body.email]
-// );
-            
-// if (rowEmail.length >= 1) {
-// return res.render('user/register', {
-//   error: 'This Email has already been used.'
-// });
-// }
-        
-// const hashPass = await bcrypt.hash(body.password, 12);
-// const pid = generateRandomNumbers(9,9).join('');
+  const { workerFirstName, workerLastName, workerMobile, workerPassword } = await req.body;
+  try {
+    // Validate inputs
+    if (workerFirstName.length < 3) {
+      return res.json({ success: false, message: 'First name must be at least 3 characters long' });
+    }
+    if (workerLastName.length < 3) {
+      return res.json({ success: false, message: 'Last name must be at least 3 characters long' });
+    }
 
-const [rows] = await dbConnection.execute(
-  "INSERT INTO `workers`(`worker_first_name`,`worker_last_name`,`worker_mobile_number`) VALUES (?,?,?)",
-  [workerFirstName, workerLastName, workerMobile]
-);
-            
-            
-if (rows.affectedRows !== 1) {
-  return res.render("Your registration has failed.")
-}
-console.log('data inserted');
-res.json({ success: true, message: 'Data inserted successfully' });
-                
-} catch (e) {
+    // Check if workerFirstName contains only letters and numbers
+    if (!/^[a-zA-Z0-9]+$/.test(workerFirstName)) {
+      return res.json({ success: false, message: 'First name must contain only letters and numbers' });
+    }
+
+    // Check if workerLastName contains only letters and numbers
+    if (!/^[a-zA-Z0-9]+$/.test(workerLastName)) {
+      return res.json({ success: false, message: 'Last name must contain only letters and numbers' });
+    }
+
+    if (workerPassword.length < 6) {
+      return res.json({ success: false, message: 'Password must be at least 6 characters long' });
+    }
+    const workerHashPassword = await bcrypt.hash(workerPassword, 12);
+    const workerPublicId = generateRandomNumbers(9, 9).join('');
+
+    const [row] = await dbConnection.execute(
+      "SELECT * FROM `workers` WHERE `worker_mobile_number`=?",
+      [workerMobile]
+    );
+
+    if (row.length >= 1) {
+      console.log('This Phone Number has already been used.');
+      return res.json({ success: false, message: 'This Phone Number has already been used.' });
+    }
+
+
+    const [rows] = await dbConnection.execute(
+      "INSERT INTO `workers`(`worker_public_id`,`worker_first_name`,`worker_last_name`,`worker_mobile_number`,`worker_password`) VALUES (?,?,?,?,?)",
+      [workerPublicId, workerFirstName, workerLastName, workerMobile, workerHashPassword]
+    );
+
+
+    if (rows.affectedRows !== 1) {
+      return res.render("Your registration has failed.")
+    }
+    console.log('data inserted');
+    res.json({ success: true, message: 'Data inserted successfully' });
+
+  } catch (e) {
     next(e);
   }
 
@@ -347,33 +206,41 @@ res.json({ success: true, message: 'Data inserted successfully' });
 
 
 // Handle POST requests for login
-app.post('/login', async (req, res) => {
-  const { workerFirstName, workerLastName, workerMobile } = req.body;
-      
+app.post('/login', async (req, res, next) => {
+  const { workerMobile, workerPassword } = await req.body;
   try {
-          
-  const [row] = await dbConnection.execute('SELECT * FROM `workers` WHERE `worker_mobile_number`=?', [workerMobile]);
-  
-  if (row.length != 1) {
-    return res.json({ success: false, message: noAccountWithThatPhoneNumberMessage});
-  }
-  
-  // const checkPass = await bcrypt.compare(body.password, row[0].password);
-  
-  // if (checkPass === true) {
-  //   req.session.userID = row[0].id;
-  //   return res.redirect('/');
-  // }
-  // Store worker information in the session
-  req.session.worker = { workerMobile };
-  res.json({ success: true, message: 'redirect user to main dashboard' });
-  // res.render('user/login', {
-  //   error: 'Credentials ERROR.'
-  // });
+    const [rows] = await dbConnection.execute('SELECT * FROM `workers` WHERE `worker_mobile_number`=?', [workerMobile]);
+
+    if (!rows || rows.length === 0) {
+      // Worker with the provided mobile number not found
+      return res.json({ success: false, message: 'Invalid mobile number or password. Please check your entries and try again' });
+    }
+
+    if (rows.length != 1) {
+      return res.json({ success: false, message: 'Invalid mobile number or password. Please check your entries and try again' });
+    }
+
+    const passwordFromDatabase = rows[0].worker_password;
+
+    if (!passwordFromDatabase) {
+      // Database entry for worker does not have a password (handle this case based on your application logic)
+      return res.json({ success: false, message: 'Invalid mobile number or password' });
+    }
+
+
+    const checkPass = await bcrypt.compare(workerPassword, passwordFromDatabase);
+
+    if (checkPass === true) {
+      req.session.worker = { workerMobile };
+      res.json({ success: true, message: 'redirect user to main dashboard' });
+    }else{
+      return res.json({ success: false, message: 'Invalid mobile number or password' });
+    }
+
   }
   catch (e) {
     next(e);
-  }  
+  }
 });
 
 // Handle GET requests to fetch data from the database
@@ -417,6 +284,114 @@ app.post('/api/data', async (req, res) => {
 io.on('connection', (socket) => {
   console.log('This client is connected to socket.io');
 });
+
+app.use(async (req, res, next) => {
+  try {
+    res.status(404).send(`<style>body {
+background-color: #2F3242;
+}
+svg {
+position: absolute;
+top: 50%;
+left: 50%;
+margin-top: -250px;
+margin-left: -400px;
+}
+.message-box {
+height: 200px;
+width: 380px;
+position: absolute;
+top: 50%;
+left: 50%;
+margin-top: -100px;
+margin-left: 50px;
+color: #FFF;
+font-family: Roboto;
+font-weight: 300;
+}
+.message-box h1 {
+font-size: 60px;
+line-height: 46px;
+margin-bottom: 40px;
+}
+.buttons-con .action-link-wrap {
+margin-top: 40px;
+}
+.buttons-con .action-link-wrap a {
+background: #68c950;
+padding: 8px 25px;
+border-radius: 4px;
+color: #FFF;
+font-weight: bold;
+font-size: 14px;
+transition: all 0.3s linear;
+cursor: pointer;
+text-decoration: none;
+margin-right: 10px
+}
+.buttons-con .action-link-wrap a:hover {
+background: #5A5C6C;
+color: #fff;
+}
+
+#Polygon-1 , #Polygon-2 , #Polygon-3 , #Polygon-4 , #Polygon-4, #Polygon-5 {
+animation: float 1s infinite ease-in-out alternate;
+}
+#Polygon-2 {
+animation-delay: .2s; 
+}
+#Polygon-3 {
+animation-delay: .4s; 
+}
+#Polygon-4 {
+animation-delay: .6s; 
+}
+#Polygon-5 {
+animation-delay: .8s; 
+}
+
+@keyframes float {
+100% {
+transform: translateY(20px);
+}
+}
+@media (max-width: 450px) {
+svg {
+position: absolute;
+top: 50%;
+left: 50%;
+margin-top: -250px;
+margin-left: -190px;
+}
+.message-box {
+top: 50%;
+left: 50%;
+margin-top: -100px;
+margin-left: -190px;
+text-align: center;
+}
+}</style><svg width="380px" height="500px" viewBox="0 0 837 1045" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+<path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Polygon-1" stroke="#007FB2" stroke-width="6" sketch:type="MSShapeGroup"></path>
+<path d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z" id="Polygon-2" stroke="#EF4A5B" stroke-width="6" sketch:type="MSShapeGroup"></path>
+<path d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z" id="Polygon-3" stroke="#795D9C" stroke-width="6" sketch:type="MSShapeGroup"></path>
+<path d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z" id="Polygon-4" stroke="#F2773F" stroke-width="6" sketch:type="MSShapeGroup"></path>
+<path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Polygon-5" stroke="#36B455" stroke-width="6" sketch:type="MSShapeGroup"></path>
+</g>
+</svg>
+<div class="message-box">
+<h1>404</h1>
+<p>Page not found</p>
+<div class="buttons-con">
+<div class="action-link-wrap">
+<a onclick="history.back(-1)" class="link-button link-back-button">Go Back</a>
+</div>
+</div>
+</div>`)
+  } catch (e) {
+    next(e);
+  }
+})
 
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Server is running on port : ${PORT}`));
---
+++ workers.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2023 at 10:51 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kenvila`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workers`
+--
+
+CREATE TABLE `workers` (
+  `id` int(11) NOT NULL,
+  `worker_public_id` varchar(200) NOT NULL,
+  `worker_first_name` varchar(50) DEFAULT NULL,
+  `worker_last_name` varchar(50) DEFAULT NULL,
+  `worker_mobile_number` varchar(30) NOT NULL,
+  `worker_password` longtext NOT NULL,
+  `Date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `workers`
+--
+ALTER TABLE `workers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `workers`
+--
+ALTER TABLE `workers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
